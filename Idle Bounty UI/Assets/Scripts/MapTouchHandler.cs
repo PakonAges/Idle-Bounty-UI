3464e086163f1630cb0ddd9a193b6bae9fa01b50
Max Okorokov <maxim.okorokov@gmail.com>
2019-07-02 23:42:48 +0300

    Island UI

---
+++ Idle Bounty UI/Assets/Scripts/MapTouchHandler.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class MapTouchHandler : MonoBehaviour
+{
+    public GameObject LevelSelectVIewPrefab;
+    private GameObject LevelSelectViewInstance;
+    RaycastHit hit;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 1000f);
+
+            if (hit.transform.tag == "MapGrid")
+            {
+                if (LevelSelectViewInstance)
+                {
+                    GameObject.Destroy(LevelSelectViewInstance);
+                }
+                else
+                {
+                    LevelSelectViewInstance = Instantiate(LevelSelectVIewPrefab, this.transform);
+                }
+            }
+        }
+    }
+}
